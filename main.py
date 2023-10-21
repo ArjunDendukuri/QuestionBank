84094536c8cfe70ebd5dffadbbab12c9f3e292f2
ArjunDendukuri <arjundendukuri@kchigh.com>
2023-10-21 18:40:21 +0530

    Made base dir for new user

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import download
+from os import path, mkdir
 import paper_manager
 import tester
 
+dirs_to_make = [
+    "downloads", "downloads\\ms", "downloads\\papers",
+    "test", "test\\ms", "test\\ms\\in", "test\\ms\\out", "test\\questions\\in", "test\\questions\\out",
+    "questions"
+]
+
+
+def make_base_dirs():
+    for dir2mk in dirs_to_make:
+        if not path.exists(dir2mk):
+            mkdir(dir2mk)
+
+
 if __name__ == '__main__':
+    make_base_dirs()
     download.run_download()
     # paper_manager.run_for_papers()
     print("Done")
